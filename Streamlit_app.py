a2d6f1f13348921f26c0ad6ebce58907cb0ee5cd
karanghai9 <37792167+karanghai9@users.noreply.github.com>
2024-10-16 02:35:06 +0200

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -3,12 +3,10 @@ import streamlit as st
 # Input for user's name
 name = st.text_input('Enter your name')
 
-# Display title with the entered name
-if name:  # This checks if a name has been entered
-    st.title(f'Hello, I am {name}')
-else:
-    st.title('Hello, I am...')
+# Display title with the entered name live while typing
+st.title(f'Hello, I am {name}')
 
 st.text("This is my first Streamlit deployment")
+
 
 
